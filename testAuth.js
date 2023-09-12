d7c3baa311ad2673aa82645b3f7793725c10178a
lynceus666 <florian.lesti@gmx.de>
2023-09-12 20:30:09 +0200

    clean up

--- testAuth.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-const myPlaintextPassword = 'meinPasswort';
-
-const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-console.log(hash);
